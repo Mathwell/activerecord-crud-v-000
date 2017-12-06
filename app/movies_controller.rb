8772d8c7ab63d55e2e70c5b29b4d599aedf7c465
Olena Ageyeva <eageeva@hotmail.com>
2017-12-06 00:56:47 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -56,7 +56,9 @@ def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
 
-  ActiveRecord::Base.connection.execute("SELECT title FROM movies WHERE release_date>=2002 ORDER BY release_date DESC")
+  ActiveRecord::Base.connection.execute("SELECT title FROM movies WHERE release_date>=2002 ORDER BY release_date DESC").each do |m|
+    m[title]
+  end
   #.map {|m| m[title]}
 end
 
